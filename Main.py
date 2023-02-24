7d8e79298f9a6cfc67c6ed7aba5b81d0f1084083
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2023-02-24 13:47:37 -0300

    VAAAAAAAAAAAAAAAAAAAApo

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import numpy as np
 from classes.Planetas import Planeta
 from classes.Ret import Ret
 from classes.Header import Header
+from classes.Menu import Menu
 import random
 
 # inicialização do Pygame
@@ -39,21 +40,7 @@ fase = {"fase":1,'corpo':Planeta(np.array([200,200]),350),"v":v,"s":s,"goal":Ret
 personagem = pygame.Surface((5, 5))  # Tamanho do personagem
 personagem.fill(COR_PERSONAGEM)  # Cor do personagem
 
-# fontes e botoes
-title_font = pygame.font.SysFont(None, 64)
-title_text = title_font.render("Smiling Foxes", True, (255, 255, 255))
-title_text_rect = title_text.get_rect()
-title_text_rect.center = (800 // 2, 600 // 3)
-
-play_button_font = pygame.font.SysFont(None, 32)
-play_button_text = play_button_font.render("Jogar", True, (255, 255, 255))
-play_button_text_rect = play_button_text.get_rect()
-play_button_text_rect.center = (800 // 2, 600 // 2)
-
-quit_button_font = pygame.font.SysFont(None, 32)
-quit_button_text = quit_button_font.render("Quit", True, (255, 255, 255))
-quit_button_text_rect = quit_button_text.get_rect()
-quit_button_text_rect.center = (800 // 2, 600 // 1.5)
+menu = Menu()
 
 background_image = pygame.image.load("./sprites/buraco.png")
 
@@ -116,16 +103,20 @@ while rodando:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             
             # se o click for no botão de inicio, mudar a página atual para "jogo"
-            if play_button_text_rect.collidepoint(event.pos):
-                pagina_atual = "jogo"
-
-            # se não for no Header, registrar o click
-            if mous_pos[1]>100 and pagina_atual == "jogo":
-                mouse_click = True
-            # se for no header, atualizar o estado do slider de força
-            elif event.button == 1:
-                # print("mouse down")
-                header.atualiza_estado()
+            if pagina_atual=="inicio":
+                if menu.atualiza_jogo(event.pos):
+                    pagina_atual = "jogo"
+                elif menu.atualiza_quit(event.pos):
+                    rodando = False
+            
+            else:
+                # se não for no Header, registrar o click
+                if mous_pos[1]>100 and pagina_atual == "jogo":
+                    mouse_click = True
+                # se for no header, atualizar o estado do slider de força
+                elif event.button == 1:
+                    # print("mouse down")
+                    header.atualiza_estado()
 
     # se houve click na tela, adicionar uma bolinha na lista e retirar um dos valores das bolinhas disponíveis (apenas funciona se houver click e bolinhas disponíveis)
     if mouse_click and b>0:
@@ -228,22 +219,13 @@ while rodando:
         # Desenhar goal
         pygame.draw.circle(screen,"WHITE",s0,5)
         pygame.draw.rect(screen,"GREEN",goal.getRect())
+
+        # Update!
         pygame.display.update()
         
 
     elif pagina_atual == "inicio":
-        screen.blit(background_image, (0, 0))
-
-        # Draw the title text
-        screen.blit(title_text, title_text_rect)
-        
-        # Draw the play button
-        pygame.draw.rect(screen, (0, 0, 0), (play_button_text_rect.left - 100, play_button_text_rect.top - 10, play_button_text_rect.width + 200, play_button_text_rect.height + 20))
-        screen.blit(play_button_text, play_button_text_rect)
-        
-        # Draw the quit button
-        pygame.draw.rect(screen, (0, 0, 50), (quit_button_text_rect.left - 100, quit_button_text_rect.top - 10, quit_button_text_rect.width + 200, quit_button_text_rect.height + 20))
-        screen.blit(quit_button_text, quit_button_text_rect)
+        menu.desenha(screen,background_image)
         
         # Update the screen
         pygame.display.flip()
@@ -261,8 +243,6 @@ while rodando:
         pagina_atual = "inicio"
         pont = 0 #pontuação
 
-    # Update!
-    
 
     # reinicialização das listas de vetores, para possibilitar a utilização em loops posteriores
     fase['v'] = v
---
+++ classes/Menu.py
@@ -0,0 +1,57 @@
+import numpy
+import pygame
+
+class Menu:
+    def __init__(self) -> None:
+        self.title_font = pygame.font.SysFont(None, 64)
+        self.title_text = self.title_font.render("Smiling Foxes", True, (255, 255, 255))
+        self.title_text_rect = self.title_text.get_rect()
+        self.title_text_rect.center = (800 // 2, 600 // 3)
+
+        self.play_button_font = pygame.font.SysFont(None, 32)
+        self.play_button_text = self.play_button_font.render("Jogar", True, (255, 255, 255))
+        self.play_button_text_rect = self.play_button_text.get_rect()
+        self.play_button_text_rect.center = (800 // 2, 600 // 2)
+
+        self.quit_button_font = pygame.font.SysFont(None, 32)
+        self.quit_button_text = self.quit_button_font.render("Quit", True, (255, 255, 255))
+        self.quit_button_text_rect = self.quit_button_text.get_rect()
+        self.quit_button_text_rect.center = (800 // 2, 600 // 1.5)
+
+        self.quit = pygame.Rect((0,0),(250,45))
+        self.quit.center = (800 // 2, 600 // 1.5)
+        self.start = pygame.Rect((0,0),(260,45))
+        self.start.center = (800 // 2, 600 // 2)
+        
+
+    def desenha(self,screen,background_image):
+
+        screen.blit(background_image, (0, 0))
+
+        # Draw the title text
+        screen.blit(self.title_text, self.title_text_rect)
+        
+        # Draw the play button
+        pygame.draw.rect(screen, (0, 0, 0), (self.play_button_text_rect.left - 100, self.play_button_text_rect.top - 10, self.play_button_text_rect.width + 200, self.play_button_text_rect.height + 20))
+        screen.blit(self.play_button_text, self.play_button_text_rect)
+        
+        # Draw the quit button
+        pygame.draw.rect(screen, (0, 0, 50), (self.quit_button_text_rect.left - 100, self.quit_button_text_rect.top - 10, self.quit_button_text_rect.width + 200, self.quit_button_text_rect.height + 20))
+        screen.blit(self.quit_button_text, self.quit_button_text_rect)
+
+        pos = pygame.mouse.get_pos()
+
+        if self.atualiza_jogo(pos):
+            pygame.draw.rect(screen,'WHITE',self.start,3)
+        elif self.atualiza_quit(pos):
+            pygame.draw.rect(screen,'WHITE',self.quit,3)
+
+    
+    def atualiza_jogo(self,pos):
+        return self.start.collidepoint(pos)
+
+    def atualiza_quit(self,pos):
+        return self.quit.collidepoint(pos)
+    
+
+
